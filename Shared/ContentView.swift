67986b2c6190a88204b0e36447d38c4f072faeb2
Rob Berrones <sellouttonight@gmail.com>
2022-07-26 05:48:38 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Rob Berrones on 7/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Karaoke_Song_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karaoke_Song_ListApp.swift
+//  Shared
+//
+//  Created by Rob Berrones on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Karaoke_Song_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
